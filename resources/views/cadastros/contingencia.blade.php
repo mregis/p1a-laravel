94c22fa4e0cea1d6b2d272ca53624bc7223e667e
Marcos Regis <marcos.regis@address.com.br>
2018-12-21 23:18:43 -0200

    * # IMPROVEMENTS #
    * Modificado Impressao de Contingencia

--- resources/views/cadastros/contingencia.blade.php
+++ resources/views/cadastros/contingencia.blade.php
@@ -24,70 +24,67 @@
 </ul>
 
 
-<div class="row">
-<div class="col-md-12">
-<div class="m-portlet m-portlet--tabs">
-
-<div class="m-portlet__body">
-
+    <div class="row">
         <div class="col-md-12">
-
-            <div class="m-portlet m-portlet--tab">
-
-                <div class="m-portlet__head">
-
-                    <div class="m-portlet__head-caption">
-
-
-                        <div class="m-portlet__head-title">
-
-                            <span class="m-portlet__head-icon m--hide">
-						        <i class="la la-gear"></i>
-						    </span>
-                            <h3 class="m-portlet__head-text">
-                                {{__('Gerar Capa por Contingência')}}
-                            </h3>
-                        </div>
-
-                    </div>
-                </div>
-                {{ Form::open(array('url' => url('api/contingencia'), 'class'=>'m-form m-form--fit m-form--label-align-right ajax-form')) }}
+            <div class="m-portlet m-portlet--tabs">
                 <div class="m-portlet__body">
-                    <div class="form-group m-form__group row">
-                        <div class="col-md-4">
-                            <label for="type">Agência Destino</label>
-                            <input type="text" class="form-control m-input" name="agency" id="agency" data-validation="notempty($(this))" data-label="{{__('labels.type')}}" data-error="{{__('labels.field_not_empty')}}" placeholder="Agência" value="">
+                    <div class="col-md-12">
+                        <div class="m-portlet m-portlet--tab">
+                            <div class="m-portlet__head">
+                                <div class="m-portlet__head-caption">
+                                    <div class="m-portlet__head-title">
+                                        <span class="m-portlet__head-icon m--hide">
+                                            <i class="la la-gear"></i>
+                                        </span>
+                                        <h3 class="m-portlet__head-text">
+                                            {{__('Gerar Capa por Contingência')}}
+                                        </h3>
+                                    </div>
+                                </div>
+                            </div>
+                            {{ Form::open(array('url' => url('api/contingencia'), 'class'=>'m-form m-form--fit m-form--label-align-right ajax-form')) }}
+                            <div class="m-portlet__body">
+                                <div class="form-group m-form__group row">
+                                    <div class="col-md-3">
+                                        <label for="type">Agência Destino</label>
+                                        <input type="text" class="form-control form-control-lg m-input" name="agency" id="agency"
+                                               data-mask="0000" required="required"
+                                               data-validation="notempty($(this))" data-label="{{__('labels.type')}}"
+                                               data-error="{{__('labels.field_not_empty')}}" placeholder="Agência"
+                                               value="">
+                                    </div>
+                                    <div class="col-md-3">
+                                        <label for="mod">Tipo de Documento</label>
+                                        <select class="form-control form-control-lg m-input m-input--square"
+                                                name="type" id="type"
+                                                required="required"
+                                                data-validation="notempty($(this))">
+                                            <option disabled selected>{{__('labels.select')}}</option>
+                                            <option value="documento"
+                                                    @if(isset($produto) && $produto->mod == 'documento') selected
+                                                    @endif;>Documento
+                                            </option>
+                                            <option value="produto"
+                                                    @if(isset($produto) && $produto->mod == 'produto') selected @endif;>
+                                                Produto
+                                            </option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="m-portlet__foot m-portlet__foot--fit">
+                                <div class="m-form__actions">
+                                    {{ Form::submit(__('GERAR CAPA'), array('class' => 'btn btn-success btn-lg')) }}
+                                </div>
+                            </div>
+                            {{ csrf_field() }}
+                            {{ Form::close() }}
                         </div>
-                        <div class="col-md-4">
-                            <label for="mod">Tipo de Documento</label>
-                            <select class="form-control m-input m-input--square" name="type" id="type" data-validation="notempty($(this))">
-                                <option disabled selected>{{__('labels.select')}}</option>
-                                <option value="documento" @if(isset($produto) && $produto->mod == 'documento') selected @endif;>Documento</option>
-                                <option value="produto" @if(isset($produto) && $produto->mod == 'produto') selected @endif;>Produto</option>
-                            </select>
-                        </div>
-
-
                     </div>
                 </div>
-                <div class="m-portlet__foot m-portlet__foot--fit">
-                    <div class="m-form__actions">
-                        {{ Form::submit(__('GERAR CAPA'), array('class' => 'btn btn-success')) }}
-                        <button type="reset" class="btn btn-secondary"
-                                onclick="window.history.back()">{{__('buttons.cancel')}}</button>
-                    </div>
-                </div>
-                {{ csrf_field() }}
-                {{ Form::close() }}
-
             </div>
-        </div>
-  </div>
-</div>
 
-</div>
-</div>
-</div>
-</div>
+        </div>
+    </div>
 
 @stop
