71704c3be26b5148cde236c580e58af643047717
Marcos Regis <marcos.regis@address.com.br>
2018-12-26 18:23:50 -0200

    * # IMPROVEMENTS #
    * Modificado CSS para corrigir hover para tabelas
    * Modificado semanticas de elementos HTML para melhorar aparencia

--- resources/views/dochistory.blade.php
+++ resources/views/dochistory.blade.php
@@ -6,21 +6,27 @@
                             class="bagde badge-primary capalote-placeholder">#CAPALOTE#</span></h4>
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
             </div>
-            <table class="table table-bordered table-striped" id="history">
-                <thead class="table-dark">
-                    <th>CAPA LOTE</th>
-                    <th>JUNÇÃO ORIGEM</th>
-                    <th>JUNÇÃO DESTINO</th>
-                    <th>DATA MOVIMENTO</th>
-                    <th>DATA ENVIO</th>
-                    <th>REGISTRO</th>
-                    <th>DATA</th>
-                    <th>USUÁRIO</th>
-                    <th>PERFIL</th>
-                    <th>LOCAL</th>
-                </thead>
-                <tbody></tbody>
-            </table>
+            <div class="row">
+                <div class="col-12">
+                    <div class="table-responsive-sm table-responsive-xl">
+                        <table class="table table-bordered table-striped table-hover nowrap" id="history">
+                            <thead class="table-dark">
+                            <th>CAPA LOTE</th>
+                            <th>JUNÇÃO ORIGEM</th>
+                            <th>JUNÇÃO DESTINO</th>
+                            <th>DATA MOVIMENTO</th>
+                            <th>DATA ENVIO</th>
+                            <th>REGISTRO</th>
+                            <th>DATA</th>
+                            <th>USUÁRIO</th>
+                            <th>PERFIL</th>
+                            <th>LOCAL</th>
+                            </thead>
+                            <tbody></tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
         <!-- /.modal-content -->
     </div>
