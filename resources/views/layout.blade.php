06485a743f601ed450993255c23815cc434cfa01
Marcos Regis <marcos.regis@address.com.br>
2019-01-09 17:16:35 -0200

    * # IMPROVEMENT #
    * Modificado Footer para uma melhor exibicao em baixas resolucoes

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1059,13 +1059,9 @@
 
 
     <!-- begin::Footer -->
-    <footer class="m-grid__item m-footer footer">
-        <div class="m-container m-container--fluid m-container--full-height m-page__container">
-            <div class="m-stack m-stack--flex-tablet-and-mobile m-stack--ver m-stack--desktop">
-                <div class="m-stack__item m-stack__item--left m-stack__item--middle m-stack__item--last">
-                    <span class="m-footer__copyright">2018 &copy; ADDRESS</span>
-                </div>
-            </div>
+    <footer class="footer">
+        <div class="container">
+            <span class="text-muted">2018 &copy; ADDRESS</span>
         </div>
     </footer>
     <!-- end::Footer -->
