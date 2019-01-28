fcc20582603558f1cb16d886bcb692e5661b018d
Marcos Regis <marcos.regis@address.com.br>
2019-01-28 17:12:46 -0200

    * #BUGFIX#
    * Removido BUG de filtro adicional (Erro de conceito)

--- app/Http/Controllers/Api/CapaLoteController.php
+++ app/Http/Controllers/Api/CapaLoteController.php
@@ -210,21 +210,7 @@ class CapaLoteController extends BaseController
                 $query->where("files.id", "=", $file_id);
             }
             if ($user->profile != 'ADMINISTRADOR') {
-                $query->where(function ($query) use ($user) {
-                    $query
-                        ->orWhere(function ($query) use ($user) {
-                            $query->where([
-                                ['files.constante', '=', 'DM'],
-                                ['docs.from_agency', '=', sprintf("%04d", $user->juncao)]
-                            ]);
-                        })
-                        ->orWhere(function ($query) use ($user) {
-                            $query->where([
-                                ['files.constante', '=', 'DA'],
-                                ['docs.to_agency', '=', sprintf("%04d", $user->juncao)]
-                            ]);
-                        });
-                });
+                $query->where('docs.to_agency', sprintf("%04d", $user->juncao));
             }
 
             return Datatables::of($query)
