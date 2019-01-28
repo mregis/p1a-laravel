204e2fedb773a6e5581f220b68a50488f4ab67b9
Marcos Regis <marcos.regis@address.com.br>
2019-01-28 19:48:22 -0200

    * # NEW FEATURE #
    * Adicionado tela de mensagem de erro para modo manutencao

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -6,12 +6,13 @@
  * Time: 13:22
  */
 ?>
+
         <!DOCTYPE html>
 
-<html lang="en" >
+<html lang="en">
 <!-- begin::Head -->
 <head>
-    <meta charset="utf-8" />
+    <meta charset="utf-8"/>
     <title>Em Manutenção</title>
     <meta name="description" content="Sistema de Controle e Rastreamento de Cheques em Trânsito">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -19,48 +20,48 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="{{ mix('css/appfill.css')}}" rel="stylesheet" type="text/css">
     <link href="{{ mix('css/appmain.css')}}" rel="stylesheet" type="text/css">
-    <link rel="shortcut icon" href="{{asset('assets/demo/default/media/img/logo/favicon.ico')}}" />
+    <link rel="shortcut icon" href="{{asset('assets/demo/default/media/img/logo/favicon.ico')}}"/>
 </head>
 <body class="m--skin- m-header--fixed m-header--fixed-mobile m-aside-left--enabled m-aside-left--skin-dark m-aside-left--offcanvas m-footer--push m-aside--offcanvas-default"
       style="background: url('{{ asset('assets/app/media/img/bg/bg-1.png') }}') 0 0 #a3c3e9;">
 <!-- begin:: Page -->
 
-    <div class="row justify-content-md-center">
+<div class="container-fluid">
+    <div class="row justify-content-md-center maintenance">
         <div class="card">
-            <div class="m-login__container">
-                <img src="{{asset('images/logo.png')}}" class="card-img-top">
-
-                <div class="m-login__head card-header">
-                    <h3 class="m-login__title">O Sistema se encontra em Manutenção</h3>
-                    <p>Volte em alguns minutos</p>
+            <div class="card-header text-center">
+                <h3><span class="text-warning">
+                        <i class="fas fa-wrench"></i>
+                    </span> Sistema em Manutenção</h3>
+            </div>
+            <div class="card-body">
+                <div class="text-center">
+                    <img src="{{ asset('/images/manutencao_md.png') }}" class="card-img-top w-75"/>
                 </div>
-                <div class="card-body">
-                    <div class="m-login__signin">
-                        <div class="card-body">
-                            <img src="{{asset('images/manutencao.jpg')}}" class="card-img-bottom">
-                        </div>
-                    </div>
+                <h4>O sistema está sendo atualizado.</h4>
+                <h5>Volte em alguns instantes para aproveitar os novos recursos que estamos preparando.</h5>
 
+                <p class="text-muted">Pedimos desculpas por qualquer incoveniente e esperamos contar com sua
+                    compreensão</p>
 
-                </div>
-                <div class="card-footer">
-                    <span class="text-muted">Address S.A - 2019 - Todos os direitos reservados</span>
-                </div>
+            </div>
+            <div class="card-footer">
+                <span class="text-muted">Address S.A - 2019 - Todos os direitos reservados</span>
             </div>
         </div>
-</div>
-<!-- Only to avoid Vue #app not found error -->
-<div id="app"></div>
+    </div>
+    <!-- Only to avoid Vue #app not found error -->
+    <div id="app"></div>
 
-<!-- end:: Page -->
-<!--begin::Base Scripts -->
-<script src="{{ mix('/js/polyfill.min.js') }}" type="text/javascript"></script>
-<script src="{{ mix('/js/manifest.js') }}" type="text/javascript"></script>
-<script src="{{ mix('/js/vendor.js') }}" type="text/javascript"></script>
-<script src="{{ mix('/js/app.js') }}" type="text/javascript"></script>
-<script src="{{ mix('/js/noCommonJS.libs.js') }}" type="text/javascript"></script>
-<script src="{{ mix('/js/custom.scripts.js') }}" type="text/javascript"></script>
-<!--end::Base Scripts -->
+    <!-- end:: Page -->
+    <!--begin::Base Scripts -->
+    <script src="{{ mix('/js/polyfill.min.js') }}" type="text/javascript"></script>
+    <script src="{{ mix('/js/manifest.js') }}" type="text/javascript"></script>
+    <script src="{{ mix('/js/vendor.js') }}" type="text/javascript"></script>
+    <script src="{{ mix('/js/app.js') }}" type="text/javascript"></script>
+    <script src="{{ mix('/js/noCommonJS.libs.js') }}" type="text/javascript"></script>
+    <script src="{{ mix('/js/custom.scripts.js') }}" type="text/javascript"></script>
+    <!--end::Base Scripts -->
 
 
 </body>
