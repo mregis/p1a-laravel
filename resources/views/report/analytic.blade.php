efec8e834614263fc436b7c30be4e9428f67160b
Marcos Regis <marcos.regis@address.com.br>
2019-02-28 17:57:07 -0300

    * # BUGFIX #
    * Corrigido bug que não ocultava a mensagem de loading de geracao de
        relatorio

--- resources/views/report/analytic.blade.php
+++ resources/views/report/analytic.blade.php
@@ -2,36 +2,36 @@
 @section('title', __('Relatório Analítico'))
 
 @section('styles')
-<style type="text/css">
-    /** SPINNER CREATION **/
+    <style type="text/css">
+        /** SPINNER CREATION **/
 
-    #loadMe .loader {
-        position: relative;
-        text-align: center;
-        margin: 15px auto 35px auto;
-        z-index: 9999;
-        display: block;
-        width: 80px;
-        height: 80px;
-        border: 10px solid rgba(0, 0, 0, .3);
-        border-radius: 50%;
-        border-top-color: #000;
-        animation: fa-spin 1.5s ease-in-out infinite;
-        -webkit-animation: fa-spin 1.5s ease-in-out infinite;
-    }
+        #loadMe .loader {
+            position: relative;
+            text-align: center;
+            margin: 15px auto 35px auto;
+            z-index: 9999;
+            display: block;
+            width: 80px;
+            height: 80px;
+            border: 10px solid rgba(0, 0, 0, .3);
+            border-radius: 50%;
+            border-top-color: #000;
+            animation: fa-spin 1.5s ease-in-out infinite;
+            -webkit-animation: fa-spin 1.5s ease-in-out infinite;
+        }
 
-    /** MODAL STYLING **/
+        /** MODAL STYLING **/
 
-    #loadMe .modal-content {
-        border-radius: 0px;
-        box-shadow: 0 0 20px 8px rgba(0, 0, 0, 0.7);
-    }
+        #loadMe .modal-content {
+            border-radius: 0px;
+            box-shadow: 0 0 20px 8px rgba(0, 0, 0, 0.7);
+        }
 
-    #loadMe .modal-backdrop.show {
-        opacity: 0.75;
-    }
+        #loadMe .modal-backdrop.show {
+            opacity: 0.75;
+        }
 
-</style>
+    </style>
 @stop
 @section('content')
     <div class="row">
@@ -67,7 +67,7 @@
                 </div>
 
                 <div class="m-portlet__body p-1">
-                    <div class="m-portlet m-portlet--tabs">
+                    <div class="m-portlet m-portlet--tabs m-1">
                         <div class="m-portlet__head">
                             <div class="m-portlet__head-tools">
                                 <ul class="nav nav-tabs m-tabs-line m-tabs-line--primary m-tabs-line--2x"
@@ -108,7 +108,8 @@
                                             <div class="input-group-prepend input-group-append">
                                                 <div class="input-group-text">Até</div>
                                             </div>
-                                            <input type="text" class="form-control datepicker" readonly="readonly" id="df"
+                                            <input type="text" class="form-control datepicker" readonly="readonly"
+                                                   id="df"
                                                    data-date-end-date="0d" data-date-autoclose="true"
                                                    value="{{date('d/m/Y')}}">
                                         </div>
@@ -119,25 +120,25 @@
                                     </div>
                                 </div>
                                 <div class="table-responsive">
-                                <table class="table table-striped table-bordered
+                                    <table class="table table-striped table-bordered
                                     responsive nowrap table-hover" id="report-analitico">
-                                    <thead class="thead-dark">
-                                    <tr>
-                                        <th>Capa de Lote</th>
-                                        <th>{{__('tables.movimento')}}</th>
-                                        <th>{{__('tables.filetype')}}</th>
-                                        <th>Cod Origem</th>
-                                        <th>Agencia Origem</th>
-                                        <th>Cod Destino</th>
-                                        <th>Agencia Destino</th>
-                                        <th>{{__('tables.status')}}</th>
-                                        <th>{{__('labels.user')}}</th>
-                                        <th>{{__('tables.profile')}}</th>
-                                        <th>{{__('tables.local')}}</th>
-                                        <th>{{__('tables.created_at')}}</th>
-                                    </tr>
-                                    </thead>
-                                </table>
+                                        <thead class="thead-dark">
+                                        <tr>
+                                            <th>Capa de Lote</th>
+                                            <th>{{__('tables.movimento')}}</th>
+                                            <th>{{__('tables.filetype')}}</th>
+                                            <th>Cod Origem</th>
+                                            <th>Agencia Origem</th>
+                                            <th>Cod Destino</th>
+                                            <th>Agencia Destino</th>
+                                            <th>{{__('tables.status')}}</th>
+                                            <th>{{__('labels.user')}}</th>
+                                            <th>{{__('tables.profile')}}</th>
+                                            <th>{{__('tables.local')}}</th>
+                                            <th>{{__('tables.created_at')}}</th>
+                                        </tr>
+                                        </thead>
+                                    </table>
                                 </div>
 
                                 <button class="btn btn-lg btn-info" type="button" onclick="exportResult();"><i
@@ -149,21 +150,22 @@
                             <div class="tab-pane" id="m_tabs_6_2" role="tabpanel">
                                 <div class="container-fluid">
                                     <table class="table table-striped table-hover table-bordered auto-dt nowrap"
-                                           data-server-side="false" data-ajax="{{route('relatorios.analytics-report-list')}}?_u={{Auth::id()}}"
+                                           data-server-side="false"
+                                           data-ajax="{{route('relatorios.analytics-report-list')}}?_u={{Auth::id()}}"
                                            data-columns='[{"data":"filename"},{"data":"params"},{"data":"username"},{"data":"created_at"},{"data":"state"},{"data":"action", "searchable":false,"orderable":false}]'
                                            id="datatable-report-list"
-                                           data-order='[[2,"asc"]]'
+                                           data-order='[[2,"asc"]]' data-processing="true"
                                            data-dom='<"row"<"col-sm-6"l><"col-sm-6"f>><"row"<"col-sm"t>><"row"<"col-sm-3"i><"col-sm-6"p>>'
                                             >
                                         <thead class="thead-dark">
-                                            <tr>
-                                                <th>Arquivo</th>
-                                                <th>Argumentos</th>
-                                                <th>Usuário</th>
-                                                <th>Data Criação</th>
-                                                <th>Estado atual</th>
-                                                <th>Ações</th>
-                                            </tr>
+                                        <tr>
+                                            <th>Arquivo</th>
+                                            <th>Argumentos</th>
+                                            <th>Usuário</th>
+                                            <th>Data Criação</th>
+                                            <th>Estado atual</th>
+                                            <th>Ações</th>
+                                        </tr>
                                         </thead>
                                     </table>
                                 </div>
@@ -190,6 +192,7 @@
                     <div class="loader"></div>
                     <div>
                         <p>Iniciando a exportação. Por favor aguarde.</p>
+
                         <p>Este processo pode levar alguns minutos
                             dependendo da quantidade de registros. Tenha paciência.</p>
                     </div>
@@ -203,13 +206,13 @@
     <script type="text/javascript">
 
         function deleteReport(id) {
-            var url = "{{route('relatorios.analytics-report-delete', [0])}}".replace("/0", "/"+id);
-            $.post(url, {"_u": "{{Auth::id()}}", "_method":"delete"}, function(response) {
+            var url = "{{route('relatorios.analytics-report-delete', [0])}}".replace("/0", "/" + id);
+            $.post(url, {"_u": "{{Auth::id()}}", "_method": "delete"}, function (response) {
                 var message = response.message ? response.message : 'Nem tudo saiu bem. Tente novamente mais tarde.';
                 $("#description_done").text(message);
                 $("#on_done_data").modal();
                 $("#datatable-report-list").DataTable().ajax.reload();
-            }).fail(function(xhr){
+            }).fail(function (xhr) {
                 var message = xhr.responseJSON && xhr.responseJSON.message ? xhr.responseJSON.message : 'Ocorreu um erro. Tente novamente mais tarde.';
                 $("#description_error").text(message);
                 $("#on_error").modal();
@@ -222,27 +225,27 @@
                     backdrop: "static", //remove ability to close modal with click
                     keyboard: false, //remove option to close with keyboard
                     show: true //Display loader!
+                }).on('shown.bs.modal', function (e) {
+                    $.post('{{route('relatorios.analytic-export')}}',
+                            report.ajax.params(),
+                            function (response) {
+                                loadme.modal('hide');
+                                if (response.data.url != null) {
+                                    window.location.href = response.data.url;
+                                } else {
+                                    var message = response.message ? response.message : 'Ocorreu um erro. Tente novamente mais tarde.';
+                                    $("#description_done").text(message);
+                                    $("#on_done_data").modal();
+                                    $("#datatable-report-list").DataTable().ajax.reload();
+                                }
+                            }, "json").fail(function (xhr) {
+                                loadme.modal('hide');
+                                var message = xhr.responseJSON && xhr.responseJSON.message ? xhr.responseJSON.message : 'Ocorreu um erro. Tente novamente mais tarde.';
+                                $("#description_error").text(message);
+                                $("#on_error").modal();
+                            });
                 });
 
-                $.post('{{route('relatorios.analytic-export')}}',
-                        report.ajax.params(),
-                        function(response) {
-                            loadme.modal('hide');
-                            if (response.data.url != null) {
-                                window.location.href = response.data.url;
-                            } else {
-                                var message = response.message ? response.message : 'Ocorreu um erro. Tente novamente mais tarde.';
-                                $("#description_done").text(message);
-                                $("#on_done_data").modal();
-                                $("#datatable-report-list").DataTable().ajax.reload();
-                            }
-                        },
-                        "json").fail(function(xhr){
-                            loadme.modal('hide');
-                            var message = xhr.responseJSON && xhr.responseJSON.message ? xhr.responseJSON.message : 'Ocorreu um erro. Tente novamente mais tarde.';
-                            $("#description_error").text(message);
-                            $("#on_error").modal();
-                        });
             }
         }
         var report = null; // Automatic Datatables
