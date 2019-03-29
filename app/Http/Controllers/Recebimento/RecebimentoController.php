e7b883ae53b1dfe0d438dac7fafca43f9cd124cc
Marcos Regis <marcos.regis@address.com.br>
2019-03-29 02:20:01 -0300

    * # BUGFIX #
    * Correcoes para permissoes de acesso a novos recursos

--- app/Http/Controllers/Recebimento/RecebimentoController.php
+++ app/Http/Controllers/Recebimento/RecebimentoController.php
@@ -10,8 +10,10 @@ namespace App\Http\Controllers\Recebimento;
 
 
 use App\Http\Controllers\BaseController;
+use App\Models\Unidade;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Auth;
+
 
 class RecebimentoController extends BaseController
 {
@@ -19,8 +21,13 @@ class RecebimentoController extends BaseController
     /**
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function operador()
+    public function operador(Request $request)
     {
+        // Acesso somente a determinados ususarios
+        if (!$unidade = Unidade::where('nome', trim(Auth::user()->unidade))->first()) {
+            $request->session()->flash('alert-danger', 'Você não tem permissão de acessar este recurso!');
+            return redirect(route('home'));
+        }
         $menus = $this->menu->menu();
         return view('recebimento.operador', compact('menus'));
     }
@@ -28,8 +35,13 @@ class RecebimentoController extends BaseController
     /**
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function carregarArquivoLeituras()
+    public function carregarArquivoLeituras(Request $request)
     {
+        // Acesso somente a determinados ususarios
+        if (!$unidade = Unidade::where('nome', trim(Auth::user()->unidade))->first()) {
+            $request->session()->flash('alert-danger', 'Você não tem permissão de acessar este recurso!');
+            return redirect(route('home'));
+        }
         $menus = $this->menu->menu();
         return view('recebimento.carregar-arquivo', compact('menus'));
     }
